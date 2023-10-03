13b1c45356ae3dd6f1d32ed549639c63ba1659dc
TylervdH <146854398+TylervdH@users.noreply.github.com>
2023-10-03 17:55:24 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -2,3 +2,30 @@
 
 
 # insert some random code
+
+
+# array
+sophi <- c(10,20,30,50,70)  # microplastics as arguments inside the vector function 
+sophi
+
+paula <- c(100, 500, 600, 1000, 2000) # people
+paula
+
+plot(paula, sophi, xlab = "no. of people", ylab = "microplastics")
+
+people <- paula
+microplastics <- sophi
+
+plot(people, microplastics, pch = 2) # this pch changes the point character to the designated symbol:number combination
+plot(people, microplastics, pch = 4, cex = 2)
+plot(people, microplastics, pch = 4, cex = 2, col = "blue")
+
+
+# PART 2
+
+install.packages("sp")
+library(sp)
+
+
+
+
