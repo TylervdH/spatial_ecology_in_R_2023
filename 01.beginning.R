85488dcf2eb0f107dc6c7d9461e2dd5ba71ea5e1
TylervdH <146854398+TylervdH@users.noreply.github.com>
2023-10-03 17:26:41 +0200

    Create 01.beginning.R

---
+++ 01.beginning.R
@@ -0,0 +1,4 @@
+# you are effectively now in an R document as you have named the file with a .R suffix, this means that the hashtag works the same and this line is not read
+
+
+# insert some random code
